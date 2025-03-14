5e29f12a1b8bdc1fa35416e53b65f2a28495916a
Moritz Beste <moritzbeste@Moritzs-MacBook-Air.local>
2025-03-14 21:37:38 +0100

    small fix

--- client.py
+++ client.py
@@ -8,25 +8,32 @@ from watchdog.events import FileSystemEventHandler
 class EventHandler(FileSystemEventHandler):
     def on_created(self, event):
         logging.info(f"File created: {event.src_path}")
+        print("created")
         notify_folder_update(b"created")
 
 
     def on_modified(self, event):
         logging.info(f"File modified: {event.src_path}")
+        print("modified")
         notify_folder_update(b"modified")
 
 
     def on_deleted(self, event):
         logging.info(f"File deleted: {event.src_path}")
+        print("deleted")
         notify_folder_update(b"deleted")
 
 
     def on_moved(self, event):
         logging.info(f"File moved: {event.src_path} -> {event.dest_path}")
+        print("moved")
         notify_folder_update(b"moved")
 
 
 def notify_folder_update(message):
+    server_ip = "10.149.106.8"
+    server_port = 5001
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, server_port))
     client_socket.sendall(message)
     client_socket.close()
@@ -55,7 +62,4 @@ def observe_sync_folder():
 
 
 if __name__ == "__main__":
-    server_port = 5001
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     observe_sync_folder()
